1924ab9821d4926d7fea929806fcd0ade5673664
OmprakashD2023 <omugan07@gmail.com>
2022-11-10 08:11:36 +0530

    Trees

--- Trees/Binary Trees/Lecture-15.cpp
+++ Trees/Binary Trees/Lecture-15.cpp
@@ -4,33 +4,64 @@
     Space Complexity : O(n) Extra space for stack
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include "Lecture-1.cpp"
 using namespace std;
 
-void iterativePreorderTraversal(TreeNode *root){
-    if(root == NULL)
-        return ;
+void iterativePreorderTraversal(TreeNode *root)
+{
+    if (root == NULL)
+        return;
     stack<TreeNode *> s;
     s.push(root);
-    while(!s.empty()){
+    while (!s.empty())
+    {
         TreeNode *node = s.top();
         s.pop();
-        cout<<node->data<<" ";
-        if(node->right!=NULL)
+        cout << node->data << " ";
+        if (node->right != NULL)
             s.push(node->right);
-        if(node->left!=NULL)
+        if (node->left != NULL)
             s.push(node->left);
     }
 }
 
-int main(){
+//Space Complexity : O(H) Since we store only the right children of the tree it can be atmost H nodes.
+void iterativePreorderTraversalEfficient(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    stack<TreeNode *> s;
+    TreeNode *curr = root;
+    while (curr != NULL || !s.empty())
+    {
+        while (curr != NULL)
+        {
+            cout << curr->data << " ";
+            if (curr->right != NULL)
+                s.push(curr->right);
+            curr = curr->left;
+        }
+        if (!s.empty())
+        {
+            curr = s.top();
+            s.pop();
+        }
+    }
+}
+
+int main()
+{
     TreeNode *root = NULL;
     BinaryTree obj = BinaryTree();
     root = obj.create();
-    cout<<"Recursive Preorder Traversal : ";
+    cout << "Recursive Preorder Traversal : ";
     obj.preorderTraversal(root);
-    cout<<endl<<"Iterative Preorder Traversal : ";
+    cout << endl
+         << "Iterative Preorder Traversal : ";
     iterativePreorderTraversal(root);
+    cout << endl
+         << "Efficient Iterative Preorder Traversal : ";
+    iterativePreorderTraversalEfficient(root);
     return 0;
 }
